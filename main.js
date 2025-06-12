80164c025a3ce15b6ac1c934b82d7d908c3ef1c9
ThucDang <85912398+DangThuc202@users.noreply.github.com>
2025-06-12 13:34:58 +0700

    5

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ console.log(storedFbAccessToken);
 
 const server = http.createServer(async (req, res) => {
   // CORS headers - luôn gửi cho mọi request
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
